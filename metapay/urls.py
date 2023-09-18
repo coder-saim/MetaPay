2a9eb27d426b4c1f32d012d275221a19d19b3573
coder-saim <saim.cse.du.744@gmail.com>
2023-09-19 01:43:45 +0600

    user authentication added

--- metapay/urls.py
+++ metapay/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path("", include("core.urls")),
+    path("user/", include("userauths.urls")),
+    # path("account/", include("account.urls")),
 ]
