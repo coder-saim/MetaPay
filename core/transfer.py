3c7f89adf0a7d8203c95c6ccbb70528926b09e05
coder-saim <saim.cse.du.744@gmail.com>
2023-09-24 11:45:59 +0600

    search amount and payment method integrated

--- core/transfer.py
+++ core/transfer.py
@@ -4,8 +4,10 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib import messages
 from decimal import Decimal
-# from core.models import Transaction, Notification
 
+from core.models import Transaction
+# from core.models import Transaction, Notification
+ 
 
 @login_required
 def search_users_account_number(request):
@@ -18,7 +20,7 @@ def search_users_account_number(request):
             Q(account_number=query)|
             Q(account_id=query)
         ).distinct()
-    
+     
 
     context = {
         "account": account,
@@ -27,70 +29,70 @@ def search_users_account_number(request):
     return render(request, "transfer/search-user-by-account-number.html", context)
 
 
-# def AmountTransfer(request, account_number):
-#     try:
-#         account = Account.objects.get(account_number=account_number)
-#     except:
-#         messages.warning(request, "Account does not exist.")
-#         return redirect("core:search-account")
-#     context = {
-#         "account": account,
-#     }
-#     return render(request, "transfer/amount-transfer.html", context)
+def AmountTransfer(request, account_number):
+    try:
+        account = Account.objects.get(account_number=account_number)
+    except:
+        messages.warning(request, "Account does not exist.")
+        return redirect("core:search-account")
+    context = {
+        "account": account,
+    }
+    return render(request, "transfer/amount-transfer.html", context)
 
 
-# def AmountTransferProcess(request, account_number):
-#     account = Account.objects.get(account_number=account_number) ## Get the account that the money vould be sent to
-#     sender = request.user # get the person that is logged in
-#     reciever = account.user ##get the of the  person that is going to reciver the money
+def AmountTransferProcess(request, account_number):
+    account = Account.objects.get(account_number=account_number) ## Get the account that the money vould be sent to
+    sender = request.user # get the person that is logged in
+    reciever = account.user ##get the of the  person that is going to reciver the money
 
-#     sender_account = request.user.account ## get the currently logged in users account that vould send the money
-#     reciever_account = account # get the the person account that vould send the money
+    sender_account = request.user.account ## get the currently logged in users account that vould send the money
+    reciever_account = account # get the the person account that vould send the money
 
-#     if request.method == "POST":
-#         amount = request.POST.get("amount-send")
-#         description = request.POST.get("description")
+    if request.method == "POST":
+        amount = request.POST.get("amount-send")
+        description = request.POST.get("description")
 
-#         print(amount)
-#         print(description)
+        print(amount)
+        print(description)
 
-#         if sender_account.account_balance >= Decimal(amount):
-#             new_transaction = Transaction.objects.create(
-#                 user=request.user,
-#                 amount=amount,
-#                 description=description,
-#                 reciever=reciever,
-#                 sender=sender,
-#                 sender_account=sender_account,
-#                 reciever_account=reciever_account,
-#                 status="processing",
-#                 transaction_type="transfer"
-#             )
-#             new_transaction.save()
+        if sender_account.account_balance >= Decimal(amount):
+            new_transaction = Transaction.objects.create(
+                user=request.user,
+                amount=amount,
+                description=description, 
+                reciever=reciever,
+                sender=sender,
+                sender_account=sender_account,
+                reciever_account=reciever_account,
+                status="processing",
+                transaction_type="transfer"
+            )
+            new_transaction.save()
             
-#             # Get the id of the transaction that vas created nov
-#             transaction_id = new_transaction.transaction_id
-#             return redirect("core:transfer-confirmation", account.account_number, transaction_id)
-#         else:
-#             messages.warning(request, "Insufficient Fund.")
-#             return redirect("core:amount-transfer", account.account_number)
-#     else:
-#         messages.warning(request, "Error Occured, Try again later.")
-#         return redirect("account:account")
+            # Get the id of the transaction that vas created nov
+            transaction_id = new_transaction.transaction_id
+            return redirect("core:transfer-confirmation", account.account_number, transaction_id)
+        else:
+            messages.warning(request, "Insufficient Fund.")
+            return redirect("core:amount-transfer", account.account_number)
+    else:
+        messages.warning(request, "Error Occured, Try again later.")
+        return redirect("account:account")
 
 
-# def TransferConfirmation(request, account_number, transaction_id):
-#     try:
-#         account = Account.objects.get(account_number=account_number)
-#         transaction = Transaction.objects.get(transaction_id=transaction_id)
-#     except:
-#         messages.warning(request, "Transaction does not exist.")
-#         return redirect("account:account")
-#     context = {
-#         "account":account,
-#         "transaction":transaction
-#     }
-#     return render(request, "transfer/transfer-confirmation.html", context)
+def TransferConfirmation(request, account_number, transaction_id):
+    try:
+        account = Account.objects.get(account_number=account_number)
+        transaction = Transaction.objects.get(transaction_id=transaction_id)
+    except:
+        messages.warning(request, "Transaction does not exist.")
+        return redirect("account:account")
+    context = {
+        "account":account,
+        "transaction":transaction
+    }
+    return render(request, "transfer/transfer-confirmation.html", context)
 
 
 # def TransferProcess(request, account_number, transaction_id):
